87ed8dd27182d3356ddc65d46673676a31098677
Matheus Monteiro <matheuschran@hotmail.com>
2022-08-17 16:36:41 +0000

    Requisito 4

--- middlewares/productNameValidator.js
+++ middlewares/productNameValidator.js
@@ -1,16 +1,17 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().required().min(5),
 });
 
 const productNameValidator = (req, res, next) => {
   const validator = schema.validate(req.body);
 
   if (validator.error) {
-    const { message } = validator.error.details[0];
+    const { message, type } = validator.error.details[0];
     const err = { message };
-    return res.status(400).json(err);
+    if (type === 'any.required') return res.status(400).json(err);
+    return res.status(422).json(err);
   }
 
   next();
